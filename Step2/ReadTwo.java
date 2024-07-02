1259cd3111a33b7ce52f946ffbaaa38c918686cb
Mohammed <mohammed@Zipcoders-MacBook-Pro-7.local>
2024-07-02 17:01:36 -0400

    fileIOJava completed

--- Step2/ReadTwo.java
+++ Step2/ReadTwo.java
@@ -4,27 +4,23 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 
-public class ReadTwo
-{
-    public static void main(String[] args)
-    {
+public class ReadTwo {
+    public static void main(String[] args) {
         try {
             Scanner fileIn = new Scanner(new File("input.txt"));
 
-            while (fileIn.hasNext()) 
-            {
+            while (fileIn.hasNext()) {
                 // Reads the entire line as a string
-                String lineIn = fileIn.nextLine(); 
+                String lineIn = fileIn.nextLine();
                 // Split the line into a String array
                 String[] input = lineIn.split(",");
                 int sum = 0;
-                for (int i = 0; i < input.length; i ++) {
+                for (int i = 0; i < input.length; i++) {
                     sum += Integer.parseInt(input[i]);
                 }
                 System.out.println(sum);
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("File not found");
         }
     }
--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -1,18 +1,54 @@
 package Step4;
 
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 
-public class ReadFour
-{
+public class ReadFour {
     /**
-     * Yes, you can look at the other 
+     * Yes, you can look at the other
      * examples in this lab to build your solution here.
      */
-        public static void main(String[] args)
-        {
-            // Print out a running total of all the
-            // values in the input file.
+    public static void main(String[] args) {
+
+//         Print out a running total of all the
+//         values in the input file.
+        //System.out.println("Working Directory = " + System.getProperty("user.dir”))
+            try {
+                Scanner fileIn = new Scanner(new File("Step4/input.txt"));
+                int sum =0;
+                while (fileIn.hasNext())
+                {
+                    // Reads the entire line as a string
+                    String lineIn = fileIn.nextLine();
+                    // Split the line into a String array
+                    String[] input = lineIn.split(",");
+
+                    for (int i = 0; i < input.length; i ++) {
+
+                        sum += Integer.parseInt(input[i]);
+
+                        System.out.println("Running total = " + sum);
+//                        PrintWriter fileOut = new PrintWriter("output.txt");
+//                        // Write text just like we would to the console
+//                        fileOut.println("Running total = " + sum);
+                        // Clode out file
+
+                    }
+//                    fileOut.close();
+                    //System.out.println(sum);
+                }
+            }
+            catch (IOException e) {
+                System.out.println("File not found");
+            }
+//            Running total = 4
+//            Runnint total = 14
+//            Running total = 17
+
+//...
         }
-}
+    };
+
+
